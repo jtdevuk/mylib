5a7417333481205876d4bc5a0e2a483a8fd28871
John Tweeddale <j.tweeddale@outlook.com>
2020-12-26 02:41:07 +0000

    search results now append to results page, yet to format

--- app/javascript/packs/books.js
+++ app/javascript/packs/books.js
@@ -1,11 +1,15 @@
 const searchGoogle = function(string) {
-  // const url = "https://www.googleapis.com/books/v1/volumes?q=search+terms"
   const searchTerms = string.replace(/ /g, "+");
-  // const search = "q=search+terms"
   fetch(`https://www.googleapis.com/books/v1/volumes?q=${searchTerms}&key=AIzaSyCl-UvKHEqZK6yAzqkwqVXJ40pOUVrBvLU`)
     .then(response => response.json())
     .then((data) => {
-      console.log(data);
+      data.items.forEach(function(element) {
+        const cardHTML = `<h3>${element.volumeInfo.title}</h3>
+                          <img src="${element.volumeInfo.imageLinks.thumbnail}"class="card-image"></img>
+                          <p>${element.volumeInfo.description}</p>`
+        const resultsDiv = document.querySelector('.search-results');      
+        resultsDiv.insertAdjacentHTML('beforeend', cardHTML);
+      })
     });
 };
 
