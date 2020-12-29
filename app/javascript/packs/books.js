42443b1df13591b6f8b979b80b1370f1e9f3ff49
John Tweeddale <j.tweeddale@outlook.com>
2020-12-29 05:51:20 +0000

    add images to book search modal, close button working

--- app/javascript/packs/books.js
+++ app/javascript/packs/books.js
@@ -6,26 +6,37 @@ const searchGoogle = function(string) {
       const resultsDiv = document.querySelector('.search-results');
       data.items.forEach(function(element) {
         const title = element.volumeInfo.title
-        const description = element.volumeInfo.description ? `${element.volumeInfo.description}...`.substring(0,200) : ""
+        // const description = element.volumeInfo.description ? `${element.volumeInfo.description}...`.substring(0,200) : ""
+        const description = element.volumeInfo.description ? `${element.volumeInfo.description}`: "";
+        const imageHTML = element.volumeInfo.imageLinks.thumbnail ? `<img src="${element.volumeInfo.imageLinks.thumbnail}"class="search-results-img">` : ``;
         const cardHTML = `<div class="book-card">
-                            <h3>${element.volumeInfo.title}</h3>
-                            <img src="${element.volumeInfo.imageLinks.thumbnail}"class="search-results-img"></img>
-                            <p>${description}</p>
+                            <h3><strong>${element.volumeInfo.title}</strong></h3>
+                            ${imageHTML}                            
                           </div>`        
         resultsDiv.insertAdjacentHTML('beforeend', cardHTML);        
       })
-      const modal = document.querySelector('.modal');
+
       const resultCards = document.querySelectorAll('.book-card');
-      resultCards.forEach((card,index) => {
-        card.addEventListener('click', function() {
-        // console.log(data.items[index].volumeInfo.title);
+      const modal = document.querySelector('.modal');
+      const modalTitle = document.querySelector('.modal-header');
+      const modalBody = document.querySelector('.modal-body');
+      const closeModal = document.querySelector('.close-modal');
+      
+      resultCards.forEach((card,index) => {        
+        const imageHTML = data.items[index].volumeInfo.imageLinks.thumbnail ? `<img src="${data.items[index].volumeInfo.imageLinks.thumbnail}"class="search-results-img">` : ``;
+        const modalHTML = `${imageHTML}
+                          <p class="modal-text">${data.items[index].volumeInfo.description}</p>`
+        card.addEventListener('click', function() {          
         modal.classList.remove('hidden');
+        modalBody.innerHTML = "";
+        modalTitle.textContent= data.items[index].volumeInfo.title;
+        modalBody.insertAdjacentHTML('beforeend', modalHTML);
+        closeModal.addEventListener('click', function() {
+          modal.classList.add('hidden');
+        })
       })
     });
   });    
 };
 
 export { searchGoogle };
-
-
-// 
